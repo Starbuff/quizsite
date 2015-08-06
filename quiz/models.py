4cde98094b7ea6aa2f599ccabe8349c8e202a420
Starbuff <maria.kareliusson@gmail.com>
2015-08-06 22:44:19 +0200

    indentation corrected

--- quiz/models.py
+++ quiz/models.py
@@ -6,7 +6,7 @@ class Quiz(models.Model):
 	slug = models.SlugField(max_length=100)
 	description = models.TextField()
 	def __unicode__(self):
-	return self.name
+		return self.name
 
 
 class Question(models.Model):
@@ -17,5 +17,5 @@ class Question(models.Model):
 	answer3 = models.CharField(max_length=100)
 	correct = models.PositiveIntegerField()
 	def __unicode__(self):
-	return self.quiz.name + " / " + self.question
+		return self.quiz.name + " / " + self.question
 
