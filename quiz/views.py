a7ecd18ced7aeda68989e8406233008ea88442a5
Starbuff <maria.kareliusson@gmail.com>
2015-08-06 12:08:07 +0200

    Updates main with quiz names

--- quiz/views.py
+++ quiz/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
-
 # coding: utf-8
+
+quizzes = {
+	"Twin_Peaks": {
+   		"name": u"Twin Peaks",
+	   	"description": u"Hur väl kommer du ihåg TV-serien?"
+	},
+	"Battlestar_Galactica": {
+	   	"name": u"Battlestar Galactica",
+	   	"description": u"Hur väl kommer du ihåg TV-serien?"
+	},
+	"House_M.D.": {
+	    	"name": u"House M.D.",
+	    	"description": u"Hur väl kommer du ihåg TV-serien?"	},
+}
+
+
 def quiz_main(request):
-	return render(request, "quiz/quiz_main.html")
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "quiz/quiz_main.html", context)
 
 def quiz_start_tp(request):
 	return render(request, "quiz/quiz_start_tp.html")
 
 def quiz_qa_tp(request):
 	return render(request, "quiz/quiz_qa_tp.html")
-	
+
 def quiz_results_tp(request):
 	return render(request, "quiz/quiz_results_tp.html")
