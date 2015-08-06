edb64c0a86a79658edddcd030448b7ca3f66d8de
Starbuff <maria.kareliusson@gmail.com>
2015-08-06 11:28:13 +0200

    Up and working

--- quiz/views.py
+++ quiz/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 
 # coding: utf-8
 def quiz_main(request):
-	return render(request, "quiz/quiz_main.html"),
+	return render(request, "quiz/quiz_main.html")
+
 def quiz_start_tp(request):
-	return render(request, "quiz/quiz_start_tp.html"),
+	return render(request, "quiz/quiz_start_tp.html")
+
 def quiz_qa_tp(request):
-	return render(request, "quiz/quiz_qa_tp.html"),
+	return render(request, "quiz/quiz_qa_tp.html")
+	
 def quiz_results_tp(request):
-	return render(request, "quiz/quiz_results_tp.html"),
+	return render(request, "quiz/quiz_results_tp.html")
