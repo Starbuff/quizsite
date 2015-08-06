5e9efc99d15a416d49e6f026e66ea4d5dc385c03
Starbuff <maria.kareliusson@gmail.com>
2015-08-06 10:15:24 +0200

    Added pages in views

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+return render(request, "quiz/quiz_main.html")
+return render(request, "quiz/quiz_start_tp.html")
+return render(request, "quiz/quiz_qa_tp.html")
+return render(request, "quiz/quiz_results_tp.html")
