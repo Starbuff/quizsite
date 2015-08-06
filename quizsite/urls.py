70e6380a461790beb02db50e8ea05641831d9fe9
Starbuff <maria.kareliusson@gmail.com>
2015-08-06 10:34:51 +0200

    Correction of URL

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -20,7 +20,7 @@ from quiz import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r"^$", views.startpage),
-    url(r"^quiz/[a-z-]+/$, views, quiz),
+    url(r"^quiz/[a-z-]+/$, views.quiz),
     url(r"^quiz/[a-z-]+/question/[0-9]/$", views.question),
     url(r"^quiz/[a-z-]+/completed/$", views.completed),
 ]
